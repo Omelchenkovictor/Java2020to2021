75821699b76862b76759b6249dbbb90cab838af2
Omelchenkovictor <58633991+Omelchenkovictor@users.noreply.github.com>
2020-12-28 10:09:16 +0200

    Add files via upload
    dz3

--- Main.java
+++ Main.java
@@ -1,17 +1,40 @@
-import java.util.Scanner;
-import java.lang.Math;
+import java.io.*;
 
 public class Main {
-    static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double a, b, c;
-        System.out.println("введіть 1 дійсне число");
-        a = Double.parseDouble(in.next());
-        System.out.println("введіть 2 дійсне число");
-        b = Double.parseDouble(in.next());
-        c = Math.sqrt( a * b );
-        System.out.format("середнє геометричне буде %e", c);
+        int n = 5, clock = 0;
+        try {
+            FileReader fr = new FileReader("C:\\Java\\projects\\Zadach5-b\\src\\Data.txt");
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            String[] arr = line.split(" ");
+            int max = 0;
+            while (clock < arr.length) {
+                 if (Integer.parseInt(arr[clock]) > max) {
+                     max = Integer.parseInt(arr[clock]);
+                     if ((clock + 1)%n == 0 ) {
+                         try {
+                             OutputStream writer = new FileOutputStream("C:\\Java\\projects\\Zadach5-b\\src\\Out.txt");
+                             String text = String.format("%d", max);
+                             //System.out.format("%d", max);
+                             writer.write(max);
+                             writer.close();
+                         }
+                         catch(IOException ex){
+
+                             System.out.println(ex.getMessage());
+                         }
+                     }
+                     //System.out.println(clock);
+                 }
+                clock ++;
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("finished");
 
     }
 }
